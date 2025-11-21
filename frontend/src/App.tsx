feb86406907c3c5189db2337954a783fa7275ade
Oreva-J <orevaawusi@gmail.com>
2025-11-21 21:47:40 +0100

    Fix TypeScript module resolution for Vercel build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Layout from './components/layout/Layout'
-import HomePage from './pages/HomePage'
-import ProductsPage from './pages/ProductsPage'
-import ProductDetailPage from './pages/ProductDetailPage'
-import AnalyticsPage from './pages/AnalyticsPage'
-import AdminPage from './pages/admin/AdminPage'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/layout/Layout.tsx";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import AdminPage from "./pages/admin/AdminPage";
 
 function App() {
   return (
@@ -13,12 +13,12 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="products" element={<ProductsPage />} />
         <Route path="products/:id" element={<ProductDetailPage />} />
-        <Route path="analytics/:id" element={<AnalyticsPage />} /> 
+        <Route path="analytics/:id" element={<AnalyticsPage />} />
         <Route path="admin/*" element={<AdminPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
